83b4fd5c04cede8332c7e12c440b0c83f07533da
YoungKey777 <pyq_bjfu@163.com>
2026-08-29 16:52:42 +0800

    daily 2026-08-29: Python 学习笔记第1-3章 + 交互模式练习

--- code/hello_python.py
+++ code/hello_python.py
@@ -1,1 +1,29 @@
-print("hello python")
+#print("hello python")
+
+
+# print("hello")
+# print(1 + 1)
+# print("量化" + "学习")
+
+
+#3.Python速览
+
+# print(17//3) 
+# print('py'*3)
+
+
+# x= int(input("please enter an integer:"))
+# if x<0:
+#     x=0
+#     print('Negative changed to zero')
+# elif x==0:
+#     print('Zero')
+# elif x==1:
+#     print('Single')
+# else:
+#     print('More')
+
+
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w)
